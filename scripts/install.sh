16f8cee359a5473310a90dc8f3ae32efc3cb9b82
zukizukizuki <zukizuki.git@gmail.com>
2023-07-29 18:35:53 +0900

    コマンドの記述がバグってたので修正

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,5 +4,5 @@ set -ex
 
 TERRAFORM_VERSION="1.5.4"
 
-wget -q “https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip”
-unzip -d /usr/local/bin “terraform_${TERRAFORM_VERSION}_linux_amd64.zip”
+wget -q "https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip"
+unzip -d "/usr/local/bin “terraform_${TERRAFORM_VERSION}_linux_amd64.zip"
