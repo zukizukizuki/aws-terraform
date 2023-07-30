4ef8696f6656b607f49511387196cb1d0eaccd65
zukizukizuki <zukizuki.git@gmail.com>
2023-07-30 15:25:12 +0900

    テスト用リソースを削除

--- sns.tf
+++ sns.tf
@@ -1,12 +1,12 @@
-resource "aws_sns_topic" "terraform_test" {
-  name = "terraform-test"
-}
+# resource "aws_sns_topic" "terraform_test" {
+#   name = "terraform-test"
+# }
 
 data "aws_s3_bucket" "zukkie_terraform_state" {
   bucket = "zukkie-terraform-state"
 }
 
-resource "aws_sns_topic" "terraform_test2" {
-  name = "terraform-test2"
-  tags = data.aws_s3_bucket.zukkie_terraform_state
-}
+# resource "aws_sns_topic" "terraform_test2" {
+#   name = "terraform-test2"
+#   tags = data.aws_s3_bucket.zukkie_terraform_state
+# }
