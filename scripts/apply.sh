9104f24a926f1485f803a7c227fb585068953e4d
zukizukizuki <zukizuki.git@gmail.com>
2023-08-08 22:53:36 +0900

    validateの追加

--- scripts/apply.sh
+++ scripts/apply.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 set -ex
 terraform init -var USERID=${userID} -input=false -no-color -backend-config="key=terraform.tfstate" -backend-config="bucket=zukkie-terraform-state"
+terraform validate -no-color
 terraform apply -auto-approve -no-color -var USERID=${userID}
