e33379fca2574ada8546d524f7cdc8f9a5bd87f4
zukizukizuki <zukizuki.git@gmail.com>
2023-08-09 21:30:11 +0900

    tflint 異常値検出テスト

--- sns.tf
+++ sns.tf
@@ -6,7 +6,7 @@ data "aws_s3_bucket" "zukkie_terraform_state" {
   bucket = "zukkie-terraform-state"
 }
 
-# resource "aws_sns_topic" "terraform_test2" {
-#   name = "terraform-test2"
-#   tags = data.aws_s3_bucket.zukkie_terraform_state
-# }
+resource "aws_sns_topic" "terraform-test2" {
+  name = "terraform-test2"
+  tags = data.aws_s3_bucket.zukkie_terraform_state
+}
